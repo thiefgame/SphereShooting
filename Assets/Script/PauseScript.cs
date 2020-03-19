25de907ba50bacaf3581167de62cf3a6ad2a5be9
thiefgame <thiefgame191016pm@gmail.com>
2020-03-19 23:28:37 +0900

    Initial commit

---
+++ Assets/Script/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    [SerializeField] GameObject PauseMenu;
+    //[SerializeField] GameObject TitleButton;
+   
+
+    public void OnPauseButtonClicked()
+    {
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+    }
+
+    public void OnContinueButtonClicked()
+    {
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+
+    public void OnTitleButtonClicked()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Title");
+    }
+
+
+}
---
+++ Assets/Script/ScoreScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ScoreScript : MonoBehaviour
+{   
+    public static int score = 0;
+
+    /*
+    void Start()
+    {
+        //シーンが終了してもこのオブジェクトは消えなくなる
+        DontDestroyOnLoad(this);
+        //シーン始めにOnSceneLoadedが走る処理
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    */
+
+    private void FixedUpdate()
+    {
+        OnScore(3);
+    }
+
+
+
+    //Mainシーンの始めに走る
+    void OnSceneLoaded(Scene Main, LoadSceneMode mode)
+    {
+        score = 0;
+    }
+
+    //メッセージを受け取る
+    void OnScore(int num)
+    {
+        //scoreに受け取った値を追加する
+        score += num;
+
+        //Textコンポーネントを取得する
+        Text Score = gameObject.GetComponent<Text>();
+
+        //scoreをテキストとして表示する
+        Score.text = "Score:" + score.ToString();
+    }
+}
+
