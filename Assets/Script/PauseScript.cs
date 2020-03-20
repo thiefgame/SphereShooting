3e7740b08f48564ccf012e02cb90205301aba0dd
doppio96 <kurotatu96@yahoo.co.jp>
2020-03-20 20:37:44 +0900

    UI追加
    汎用テキストを追加

--- Assets/Script/PauseScript.cs
+++ Assets/Script/PauseScript.cs
@@ -7,21 +7,22 @@ using UnityEngine.SceneManagement;
 public class PauseScript : MonoBehaviour
 {
     [SerializeField] GameObject PauseMenu;
-    //[SerializeField] GameObject TitleButton;
-   
 
+    //ポーズボタンを押したとき時間を止める
     public void OnPauseButtonClicked()
     {
         Time.timeScale = 0f;
         PauseMenu.SetActive(true);
     }
 
+    //コンティニューボタンを押したとき時間を動かす
     public void OnContinueButtonClicked()
     {
         Time.timeScale = 1f;
         PauseMenu.SetActive(false);
     }
 
+    //タイトルボタンを押したとき時間を動かしてタイトルに飛ぶ
     public void OnTitleButtonClicked()
     {
         Time.timeScale = 1f;
---
+++ Assets/Script/UITextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextController : MonoBehaviour
+{
+    [SerializeField] GameObject UIText;
+
+    void Start()
+    {
+        StartCoroutine("TextSetActive");
+    }
+
+    //UITextのアクティブ状態を制御する
+    IEnumerator TextSetActive()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        UIText.SetActive(false);
+    }
+
+    //UITextの内容を書き換えるメソッド
+    void ChangeUIText(string text)
+    {
+        UIText.GetComponent<Text>().text = text;
+    }
+}
