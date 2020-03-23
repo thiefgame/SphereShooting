9c210b9c8089de9b76a14f2a8526b1ed8c49002f
doppio96 <55580720+doppio96@users.noreply.github.com>
2020-03-23 16:55:49 +0900

    ボスライフ実装

--- Assets/Script/BOSS1Gimmick.cs
+++ Assets/Script/BOSS1Gimmick.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BOSS1Gimmick : MonoBehaviour
 {
     [SerializeField] GameObject Tentacls;
     GameObject Obj;
- 
+    float BOSSLife = 1.0f;
+    //[SerializeField] GameObject Slider;
+    //Slider BOSSSlider;
+    [SerializeField] Slider BOSSSlider;
+
     void Start()
     {
         StartCoroutine("TentaclsInstantiate");
+        //BOSSSlider = Slider.GetComponent<Slider>();
     }
 
     void FixedUpdate()
     {
         transform.Rotate(new Vector3(0, 1, 0));
+        BOSSSlider.value = BOSSLife;
     }
 
     IEnumerator TentaclsInstantiate()
@@ -25,6 +32,13 @@ public class BOSS1Gimmick : MonoBehaviour
             Obj = Instantiate(Tentacls, this.transform.position, Quaternion.identity);
             Obj.transform.parent = this.transform;
         }
+    }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            BOSSLife -= 1;
+        } 
     }
 }
